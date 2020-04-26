3fe97f86d0d305e0f0eb5d2b865b548a8784a43a
Danil Shvalov <shvalovich@gmail.com>
2020-04-27 08:08:59 +1000

    Добавьте файлы проекта.

---
+++ FunWithObservable/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithObservable
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person() { }
+        public Person(string name, string surname, int age)
+        {
+            FirstName = name;
+            LastName = surname;
+            Age = age;
+        }
+        public override string ToString() => $"Name: {FirstName} {LastName}, Age: {Age}";
+    }
+}
---
+++ FunWithObservable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace FunWithObservable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<Person> people = new ObservableCollection<Person>()
+            {
+                new Person(){ FirstName = "Peter", LastName = "Murphy", Age = 52},
+                new Person(){ FirstName = "Kevin", LastName = "Key", Age = 48},
+            };
+
+            people.CollectionChanged += people_CollectionChanged;
+
+            people.Add(new Person() { FirstName = "Fred", LastName = "Jonnat", Age = 40 });
+
+            Console.ReadLine();
+        }
+        static void people_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine($"Action for this event: {e.Action}");
+
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Here are OLD items:");
+                foreach(Person item in e.OldItems)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine();
+            }
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Here are NEW items:");
+                foreach (Person item in e.NewItems)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
